b9d8548d26aa063f269e6567e2e92f0c5e819a45
유지한 <mijisoo97@naver.com>
2026-05-26 20:58:43 +0900

    feat: 상품 구현

---
+++ src/main/java/com/example/potatochip/product/entity/product.java
@@ -0,0 +1,16 @@
+package com.example.potatochip.product.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class product {
+    private int id;
+    private int seller_id;
+
+
+}
+
