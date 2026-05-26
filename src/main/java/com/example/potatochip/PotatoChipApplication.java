6e60a5f01971ab73dca98cfc0c2cba1ae726e634
유지한 <mijisoo97@naver.com>
2026-05-26 19:55:32 +0900

    init: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/example/potatochip/PotatoChipApplication.java
@@ -0,0 +1,13 @@
+package com.example.potatochip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoChipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatoChipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/potatochip/PotatoChipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.potatochip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoChipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
