3f76064f2a0e92a9081355ee30c8389bb4ee2287
ZERO-1018-26 <yz926817@gmail.com>
2026-06-05 21:58:06 +0900

    feat:게시판 CRUD 구현

--- src/main/java/com/example/potatochip/product/controller/BoardController.java
+++ src/main/java/com/example/potatochip/product/controller/BoardController.java
@@ -1,12 +1,50 @@
 package com.example.potatochip.product.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.potatochip.product.dto.request.BoardRequestDTO;
+import com.example.potatochip.product.dto.response.BoardResponseDTO;
+import com.example.potatochip.product.entity.Board;
+import com.example.potatochip.product.repository.BoardRepository;
+import com.example.potatochip.product.service.BoardService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
 
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+    @GetMapping
+    public List<BoardResponseDTO> getBoard() {
+        return boardService.getBoards();
+    }
+
+    @GetMapping("/{id}")
+    public BoardResponseDTO getBoard(@PathVariable Long id) {
+        return boardService.getBoard(id);
+    }
+
+    @PostMapping
+    public BoardResponseDTO createBoard(@RequestBody BoardRequestDTO request){
+        return boardService.createBoard(request);
+    }
+
+    @PostMapping("/{id}")
+    public BoardResponseDTO updateBoard(
+            @PathVariable Long id,
+            @RequestBody BoardRequestDTO request) {
+
+        return boardService.updateBoard(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBoard(@PathVariable Long id) {
+        boardService.deleteBoard(id);
+    }
 
 }
