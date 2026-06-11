12235f4cb70e610c7c7c49b7d4074bc7289c0f13
ZERO-1018-26 <yz926817@gmail.com>
2026-06-11 17:54:32 +0900

    지금까지 개발

---
+++ src/main/java/com/example/potatochip/board/dto/BoardRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.potatochip.board.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardRequestDTO {
+    private String title;
+    private String content;
+}
--- src/main/java/com/example/potatochip/order/controller/OrderController.java
+++ src/main/java/com/example/potatochip/order/controller/OrderController.java
@@ -1,9 +1,11 @@
 package com.example.potatochip.order.controller;
 
 import com.example.potatochip.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
 
