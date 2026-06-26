c0f91704b85ced99a6fd6ee322007f95eff9965e
유지한 <mijisoo97@naver.com>
2026-06-26 19:29:11 +0900

    farmer에 상품 등록 추가

--- src/main/java/com/example/potatochip/product/service/ranking/ProductRankingsServiceImpl.java
+++ src/main/java/com/example/potatochip/product/service/ranking/ProductRankingsServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
